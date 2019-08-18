c34a10711c536f71c80d8bcb82ac91ffa7796dd1
Lars Wächter <lars97.waechter@gmail.com>
2019-08-18 18:00:50 +0200

    Enable board sharing

---
+++ src/components/Board/OptionButtons.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import ReactModal from 'react-modal';
+import { Link } from 'react-router-dom';
+
+import MiscShare from 'components//Misc/Share';
+
+const BoardOptionButtons = (props) => {
+	const { board } = props;
+
+	const [showModal, setShowModal] = useState(false);
+	const [modalContent, setModalContent] = useState(null);
+
+	const openShareModal = () => {
+		setModalContent(<MiscShare endpoint={`boards/${board.id}/share`} />);
+		setShowModal(true);
+	};
+
+	const handleCloseModal = () => {
+		setShowModal(false);
+	};
+
+	return (
+		<div className="BoardOptionButtons">
+			<div className="btn-group ml-2">
+				<button
+					type="button"
+					className="btn btn-primary dropdown-toggle"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false"
+				>
+					More
+				</button>
+				<div className="dropdown-menu dropdown-menu-right">
+					<button type="button" className="btn dropdown-item" onClick={openShareModal}>
+						<i className="fas fa-share fa-fw mr-2" /> Share
+					</button>
+					<button type="button" className="btn dropdown-item">
+						<i className="fas fa-print fa-fw mr-2" /> Print
+					</button>
+					<button type="button" className="dropdown-item">
+						<i className="fas fa-archive fa-fw mr-2" /> Archive
+					</button>
+					<div className="dropdown-divider" />
+					<button type="button" className="btn dropdown-item text-danger">
+						<i className="fas fa-trash fa-fw mr-2" /> Delete
+					</button>
+				</div>
+			</div>
+			<ReactModal
+				isOpen={showModal}
+				contentLabel="Minimal Modal Example"
+				className="Modal"
+				overlayClassName="Modal-Overlay"
+			>
+				<div className="modal-header">
+					<h5 className="modal-title">Share board</h5>
+					<button type="button" className="close" aria-label="Close" onClick={handleCloseModal}>
+						<span aria-hidden="true">×</span>
+					</button>
+				</div>
+				<div className="modal-body">{modalContent}</div>
+			</ReactModal>
+		</div>
+	);
+};
+
+BoardOptionButtons.defaultProps = {
+	assignedClasses: []
+};
+
+export default BoardOptionButtons;
--- src/sites/Boards/Board/index.js
+++ src/sites/Boards/Board/index.js
@@ -7,6 +7,7 @@ import InputTitle from 'components/UI/Input/Title/';
 
 import KanbanContainer from 'components/Kanban/container';
 import BoardDetails from 'components/Board/Details';
+import BoardOptionButtons from 'components/Board/OptionButtons';
 
 const SitesBoard = (props) => {
 	const { board, updateParentBoardState, deleteBoard } = props;
@@ -33,31 +34,8 @@ const SitesBoard = (props) => {
 							deleteBoard={deleteBoard}
 							classes={['d-inline-block']}
 						/>
-						<div className="btn-group ml-2">
-							<button
-								type="button"
-								className="btn btn-primary dropdown-toggle disabled"
-								data-toggle="dropdown"
-								aria-haspopup="true"
-								aria-expanded="false"
-							>
-								More
-							</button>
-							<div className="dropdown-menu dropdown-menu-right">
-								<button type="button" className="btn dropdown-item">
-									<i className="fas fa-share fa-fw mr-2" /> Share
-								</button>
-								<button type="button" className="btn dropdown-item">
-									<i className="fas fa-print fa-fw mr-2" /> Print
-								</button>
-								<button type="button" className="dropdown-item">
-									<i className="fas fa-archive fa-fw mr-2" /> Archive
-								</button>
-								<div className="dropdown-divider" />
-								<button type="button" className="btn dropdown-item text-danger">
-									<i className="fas fa-trash fa-fw mr-2" /> Delete
-								</button>
-							</div>
+						<div className="btn-group">
+							<BoardOptionButtons board={board} />
 						</div>
 					</div>
 				</div>
